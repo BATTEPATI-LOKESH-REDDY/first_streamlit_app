e153f4c08f1c38bb3eb34e8c0bc02baf021f72f2
BATTEPATI-LOKESH-REDDY <125631854+BATTEPATI-LOKESH-REDDY@users.noreply.github.com>
2023-07-10 12:14:14 +0530

    Update streamlit_app.py
    title edited

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healty Diner')
+streamlit.title('My Mom's New Healty Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
