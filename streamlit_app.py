213399a25dfa173aa74556f3d0e9bde8cecffd8d
BATTEPATI-LOKESH-REDDY <125631854+BATTEPATI-LOKESH-REDDY@users.noreply.github.com>
2023-07-10 12:09:54 +0530

    Update streamlit_app.py
    added breakfast header menu and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 and blueberry Oatmeal')
-streamlit.text('kale , Spanish & Rocket smoothie')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
